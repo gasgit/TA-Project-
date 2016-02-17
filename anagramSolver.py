b2dba987b8beab6005a6cc88ae93c0333d933e10
gasgit <g00305555@gmit.ie>
2016-02-17 22:36:59 +0000

    try refomat code

--- anagramSolver.py
+++ anagramSolver.py
@@ -5,31 +5,31 @@
 
 
 def createAnagramsArray():
-	w=0
-	for words in fr:
-		w+=1
-		for letter in inputLetters:
-			if letter not in words:
-				break
-		else:
-			WordsContainingLetters.append(words.strip())
-	print(WordsContainingLetters)
-	print('Total Words in file: ', w)
-	print('WordsContainingLetters:',len(WordsContainingLetters))
+    w = 0
+    for words in fr:
+        w += 1
+        for letter in inputLetters:
+            if letter not in words:
+                break
+        else:
+            WordsContainingLetters.append(words.strip())
+    print(WordsContainingLetters)
+    print('Total Words in file: ', w)
+    print('WordsContainingLetters:', len(WordsContainingLetters))
 
 
 def createResultsArray():
-	result = []
-	for w in WordsContainingLetters:
-	    for letter in w:
-	        if letter not in inputLetters:
-	            break
-	    else:
-	        result.append(w.strip())
-	print('Actual Anagrams: ', result)
+    result = []
+    for w in WordsContainingLetters:
+        for letter in w:
+            if letter not in inputLetters:
+                break
+        else:
+            result.append(w.strip())
+    print('Actual Anagrams: ', result)
 
 
-fr = open('words.txt','r')
+fr = open('words.txt', 'r')
 inputLetters = input('Enter letters: ')
 WordsContainingLetters = []
 createAnagramsArray()
