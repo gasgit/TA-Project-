5bce765ddfda30270ee3324f3eb76dc4161d4935
gasgit <g00305555@gmit.ie>
2016-02-18 18:30:45 +0000

    rename filio to anagramSolver2, want to keep  iterations numbered for records

---
+++ anagramSolver2.py
@@ -0,0 +1,54 @@
+from itertools import permutations
+
+#do some stuff
+
+
+inputLetters = input('Enter letters: ')
+
+
+perms = []
+results = []
+
+def getPerms():
+    for perm in permutations(inputLetters):
+        perms.append( "".join(perm))
+    print(len(perms))
+    #print(perms)
+
+    setPerms = set(perms)
+    #print(setPerms)
+
+    fr = open('uk.txt', 'r')
+    content = fr.read()
+    contents = content.split()
+    setContents = set(contents)
+
+    for word in setPerms:
+        for w in setContents:
+            if word not in setContents:
+                break
+
+        else:
+            results.append(word.strip())
+
+            print('Perms: ',len(perms))
+            print(results)
+
+
+
+getPerms()
+#timeit.timeit(getPerms)
+
+
+
+
+
+
+
+
+
+
+#
+#
+#
+# print(contents)
