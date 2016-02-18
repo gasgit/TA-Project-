0b1834cc7023602bb85361102541e2f081e70fd7
gasgit <g00305555@gmit.ie>
2016-02-18 18:04:21 +0000

    compare sets, for else looping at length if input

--- fileio.py
+++ fileio.py
@@ -1,8 +1,50 @@
-fr = open('uk.txt', 'r')
-content = fr.read()
-contents = content.split()
+from itertools import permutations
 
-for i in contents:
-    print(i)
 
-#print(contents)
+inputLetters = input('Enter letters: ')
+
+
+perms = []
+results = []
+
+def getPerms():
+    for perm in permutations(inputLetters):
+        perms.append( "".join(perm))
+    print(len(perms))
+    #print(perms)
+
+    setPerms = set(perms)
+    #print(setPerms)
+
+    fr = open('uk.txt', 'r')
+    content = fr.read()
+    contents = content.split()
+    setContents = set(contents)
+
+    for word in setPerms:
+        for w in setContents:
+            if word not in setContents:
+                break
+
+        else:
+            results.append(word.strip())
+
+            print('Perms: ',len(perms))
+            print(results)
+
+
+getPerms()
+
+
+
+
+
+
+
+
+
+
+#
+#
+#
+# print(contents)
